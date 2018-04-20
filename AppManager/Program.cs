013d360580d6ccf709d65db2b1448e74e1605f51
Camara Ibrahima <camara@eurisnet.it>
2018-04-20 15:11:44 +0200

    -Added RabbitReceiver console, and tested to send a ncd receive a message:result->ok!

--- AppManager/Program.cs
+++ AppManager/Program.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using FromMongoToRabbit;
+using RabbitMQ.Client;
 
 namespace AppManager
 {
@@ -18,7 +19,7 @@ namespace AppManager
             kernel.Bind<IDbRepository>().To<MongoRepository>();
 
             var mongodb = kernel.Get<IDbRepository>();
-            mongodb.Add<Product>(new Product {
+            /*mongodb.Add<Product>(new Product {
                 Name = "Prodotto1",
                 Description = "ProvaDescrizione bla bla vla",
                 Price = 300.00,
@@ -28,14 +29,38 @@ namespace AppManager
                     Description = "Descrizione Catalogo",
                     ExpirationDate = new DateTime(2019, 5, 23)
                 }
-            
-            });
+            });*/
             foreach(Product product in mongodb.All<Product>().ToList())
             {
                 Console.WriteLine(product.Name+" "+product.Description + " appartiene al catalogo:"+ product.PriceList.Name);
             }
-            
+
+            IList<Product> productList = mongodb.All<Product>().ToList();
+
+
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName="guest", Password="guest" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "FirstQueueIbra",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = productList.First().Description.ToString();
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "IbraExchange",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
+            
         }
     }
 }
